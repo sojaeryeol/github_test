84b557260253b059fe0e073f42c422c43ddaf621
sojaeryeol <sjr711203@gmail.com>
2024-06-25 12:13:42 +0900

    이름

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(49, 48);
+            this.button1.Location = new System.Drawing.Point(59, 45);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(206, 70);
             this.button1.TabIndex = 0;
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            label1.Text = "소재열";  
+            label1.Text = "재열";  
         }
     }
 }
